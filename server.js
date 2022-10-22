94c5db48d50ab4bc9f541840c073cdf92e8c4fdf
Kelly McDonald <kellymcdonald@Admins-MacBook-Air.local>
2022-10-22 01:00:13 -0700

    css and stats

--- server.js
+++ server.js
@@ -7,31 +7,44 @@ app.use(express.json())
 app.use(express.static("public"))
 
 // follow I N D U C E
-
+//index
 app.get("/pokemon", (req, res) => {
     res.render("index.ejs", {
         pokemon: pokemon
     })
     console.log(pokemon)
 })
-
+//new
 app.get("/pokemon/new", (req, res) => {
     res.render("new.ejs")
 })
 
-app.get("/pokemon/:id", (req, res) => {
-  res.render("show.ejs", {
-    pokemon : pokemon[req.params.id]
-  })
-})  
 
+
+//delete
+
+// update
+//placeholder for the update route
+
+ 
+//create
 app.post("/pokemon", (req, res) => {
     console.log(req.body)
     pokemon.push (req.body)
-        res.redirect("/pokemon/")
+        res.redirect("/pokemon")
 })
 
-console.log(pokemon)
+//edit
+// show
+
+//show
+app.get("/pokemon/:id", (req, res) => {
+    res.render("show.ejs", {
+      pokemon : pokemon[req.params.id]
+    })
+  }) 
+
+
 
 
 
